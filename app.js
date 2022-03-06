2e9257b7f1055a1ad273acaa3aca6c0eb605caeb
teodor <teodor300ia@gmail.com>
2022-03-06 10:44:01 +0200

    UPLOAD

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization,content-type,application/json"
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization,content-type"
   );
   next();
 });
