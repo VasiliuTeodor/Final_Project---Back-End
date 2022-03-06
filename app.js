bdd66fee0c1c66c963f58a4d7181a761f3845468
teodor <teodor300ia@gmail.com>
2022-03-06 10:29:22 +0200

    upload

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const app = express();
 
 // solve CORS issues for localhost requests
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://bookworm-shop.herokuapp.com/products"
+  );
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
