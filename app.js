649ed071e6b9dfa5ab2bbc4ded12e5275cde96cd
teodor <teodor300ia@gmail.com>
2022-03-06 10:42:33 +0200

    upload

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const app = express();
 
 // solve CORS issues for localhost requests
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization,content-type,application/json"
   );
